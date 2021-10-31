4b8781e840fcb29aa9fc185858ec60143af192b6
Yu Ke <yu.ke.name@gmail.com>
2021-10-31 22:15:19 +0800

    Add implement.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# apple-client-secret
-Generate client secret for Apple get token call
+# Generate client secret for Apple get token call
+A util to generate client secret used in Apple get token call.
+
+Create a config.json file with the following information:
+```json
+{
+	"service_id": "<service_id>",
+	"key_id": "<key_id>",
+	"team_id": "<team_id>"
+}
+```
+Place private key under root folder as `apple.p8`:
+```
+-----BEGIN PRIVATE KEY-----
+<private key here>
+-----END PRIVATE KEY-----
+```
+And then run `go run main.go`
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type Config struct {
+	ServiceID string `json:"service_id"`
+	KeyID     string `json:"key_id"`
+	TeamID    string `json:"team_id"`
+}
+
+type AppleClientSecret struct {
+	Issuer   string `json:"iss"`
+	IssueAt  int64  `json:"iat"`
+	Expire   int64  `json:"exp"`
+	Audience string `json:"aud"`
+	Sub      string `json:"sub"`
+}
+
+func (a AppleClientSecret) Valid() error {
+	return nil
+}
+
+func getConfiguration() (*Config, error) {
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	err = json.Unmarshal(data, config)
+	return config, err
+}
+
+func main() {
+	var err error
+	conf, err := getConfiguration()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	privateBytes, err := ioutil.ReadFile("apple.p8")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	privateKey, err := jwt.ParseECPrivateKeyFromPEM(privateBytes)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	token := jwt.New(jwt.GetSigningMethod("ES256"))
+
+	token.Header = map[string]interface{}{
+		"kid": conf.KeyID,
+		"alg": "ES256",
+	}
+
+	payload := AppleClientSecret{
+		Issuer:   conf.TeamID,
+		IssueAt:  time.Now().Unix(),
+		Expire:   time.Now().Add(60 * time.Minute).Unix(),
+		Audience: "https://appleid.apple.com",
+		Sub:      conf.ServiceID,
+	}
+	token.Claims = payload
+
+	signature, err := token.SignedString(privateKey)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(signature)
+}
